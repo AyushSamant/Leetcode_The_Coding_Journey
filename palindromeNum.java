af9b92e3e32a0492ee8652dadc1fa3e90e1f7f69
AyushSamant <148461798+AyushSamant@users.noreply.github.com>
2024-07-27 10:40:03 +0530

    Add files via upload

---
+++ palindromeNum.java
@@ -0,0 +1,29 @@
+//Problem - Check whether the number is palindrome or not
+
+import java.util.Scanner;
+
+public class palindromeNum {
+    public void reverseInt(int number){               //method to check palindrome number with a single parameter
+        int rev=0;
+        int orignal=number;                           //Storing number in other variable
+
+        while(number!=0){
+            int digit=number%10;
+            rev=(rev*10)+digit;
+            number/=10;
+        }
+
+        if(orignal==rev){
+            System.out.println("Result: It is a palindrome number");
+        }else{
+            System.out.println("Result: It is not a palindrome number");
+        }
+    }
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter the number: ");
+        int number=sc.nextInt();                        //Taking input
+        palindromeNum num1=new palindromeNum();      //creating object
+        num1.reverseInt(number);                       //calling method with number as arguement
+    }
+}
