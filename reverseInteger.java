2a00d4d7fdd78cbc288afa2e42415f1c071ffe77
AyushSamant <148461798+AyushSamant@users.noreply.github.com>
2024-07-27 10:47:25 +0530

    Update reverseInteger.java

--- reverseInteger.java
+++ reverseInteger.java
@@ -1,30 +1,25 @@
-//Problem - Check whether the number is palindrome or not
+//Problem - Reverse a given Integer
 
 import java.util.*;
 import java.util.Scanner;
 
-public class reverseInteger {
-    public void reverseInt(int number){               //method to check palindrome number with a single parameter
+public class reverseInt {
+    static  public int reverse(int num){
         int rev=0;
-        int orignal=number;                           //Storing number in other variable
+        int orignal=num;
 
-        while(number!=0){
-            int digit=number%10;
+        while(num!=0){
+            int digit=num%10;
             rev=(rev*10)+digit;
-            number/=10;
+            num/=10;
         }
-
-        if(orignal==rev){
-            System.out.println("Result: It is a palindrome number");
-        }else{
-            System.out.println("Result: It is not a palindrome number");
-        }
+        return rev;
     }
     public static void main(String args[]){
         Scanner sc=new Scanner(System.in);
         System.out.print("Enter the number: ");
-        int number=sc.nextInt();                        //Taking input
-        reverseInteger num1=new reverseInteger();      //creating object
-        num1.reverseInt(number);                       //calling method with number as arguement
+        int number=sc.nextInt();
+        int result=reverse(number);
+        System.out.println("The reverse is: "+result);
     }
 }
